88e5f22b00744427ce22c7319e05b7afad6e1713
Jagdish Patil <130397851+jagdishpatil07@users.noreply.github.com>
2025-08-10 16:36:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
+  description = "The name of the SSH key pair to use for EKS worker nodes"
   type        = string
-  default     = "KEY_AWS" # AWS Key Pair name (without .pem)
+  default     = "KEY_AWS"
 }
