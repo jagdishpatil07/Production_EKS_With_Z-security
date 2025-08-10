4a838c937e5f9e458a2bfbbcb10bb4ea7401b13d
Jagdish Patil <130397851+jagdishpatil07@users.noreply.github.com>
2025-08-10 16:37:08 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
   value = aws_eks_cluster.jagdish.id
 }
-
 output "node_group_id" {
   value = aws_eks_node_group.jagdish.id
 }
-
 output "vpc_id" {
   value = aws_vpc.jagdish_vpc.id
 }
-
 output "private_subnet_ids" {
   value = aws_subnet.private_subnet[*].id
+}
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnet[*].id
 }
